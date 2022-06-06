37896d256243217dac2ee9a95fedb74abc509371
FelipeMeireles25032003 <felipe.meireles825@gmail.com>
2022-06-06 19:03:32 -0300

    <aba Informações adicionadas>

---
+++ JovemProgramadorMvs/Controllers/InformacaoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JovemProgramadorMvs.Controllers
+{
+    public class InformacaoController : Controller
+    {
+        public InformacaoController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
